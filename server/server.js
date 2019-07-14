81e0891c5fb735c045fb65f2eb5455e5ffba46e8
lepfsd-strapp <lperez@master.inmobilia.com>
2019-07-14 17:40:50 -0400

    validators types

---
+++ server/server.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+	text: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	},
+	completed: {
+		type: Boolean,
+		default: false
+	},
+	completedAt: {
+		type: Number,
+		default: null
+	}
+});
+
+/*var newTodo = new Todo({
+	text: 'Cook dinner'
+});
+
+newTodo.save().then((doc) => {
+	console.log('saved todo', doc);
+}, (e) => {
+	console.log('unable  to save todo');
+}); */
+
+/*var otherTodo = new Todo({
+	text: 'feed the cat',
+	completed: true,
+	completedAt: 123
+});*/
+
+/*otherTodo.save().then((doc) => {
+	console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+	console.log('unable  to save todo');
+}); */
+
+var User = mongoose.model('User', {
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 1
+	}
+});
+
+var user = new User({
+	email: "lepfsd@gmail.com"
+});
+
+user.save().then((doc) => {
+	console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+	console.log('unable  to save todo');
+});
+
+
