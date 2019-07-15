79b32b4f9184e01a821a64e272f1dc64d2b2ec7d
lepfsd-strapp <lperez@master.inmobilia.com>
2019-07-14 21:28:57 -0400

    get find by i

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -25,6 +26,22 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		return res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	})
 });
 
 app.listen(3000, () => {
