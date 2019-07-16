f708f9b6ad58454ef0cb3bd9d3230e7fbd3f3a0f
lepfsd-strapp <lperez@master.inmobilia.com>
2019-07-15 21:36:36 -0400

    update a resource

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -59,6 +60,32 @@ app.delete('/todos/:id', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send();
 	});
+});
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']); 
+	if(!ObjectID.isValid(id)) {
+		return status(400).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+		.then((todo) => {
+			if(!todo) {
+				return res.status(404).send();
+			}
+			res.send({todo});
+		})
+		.catch((e) => {
+			res.status(400).send();
+		});
 });
 
 app.listen(port, () => {
