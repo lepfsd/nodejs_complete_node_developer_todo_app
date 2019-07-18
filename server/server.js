9c8773d074cbf6f5c898c57ffeae61cdfffa7175
lepfsd-strapp <lperez@master.inmobilia.com>
2019-07-18 00:58:19 -0400

    post users method

--- server/server.js
+++ server/server.js
@@ -88,6 +88,17 @@ app.patch('/todos/:id', (req, res) => {
 		});
 });
 
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+
+	user.save().then((user) => {
+		res.send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
 });
